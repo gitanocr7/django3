908c465f9b63933e212859fb4bd3f9c7b47e240e
Diegorr <diegoroblesrivera@gmail.com>
2024-06-03 15:42:22 -0400

    Django para rogramacion WEB

---
+++ alumnos/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('index', views.index, name='index'),
+    path('listadoSQL', views.listadoSQL, name='listadoSQL'),
+    path('crud', views.crud, name='crud'),
+    path('alumnosAdd', views.alumnosAdd, name='alumnosAdd'),
+    # path('alumnos_del', views.alumnos_del, name='alumnos_del'),
+    # path('alumnos_findEdit', views.alumnos_findEdit, name='alumnos_findEdit'),
+]
